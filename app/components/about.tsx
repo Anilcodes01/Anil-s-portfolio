821f52a1854fd2badd447479cd2440e4a6c545f5
Anil Pal <anilcodes01@gmail.com>
2024-11-04 12:47:11 +0530

    fixed the bug

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -5,8 +5,8 @@ type AboutProps = {
 export default function About({ onProjectsClick }: AboutProps) {
   return (
     <div>
-      <div className="text-lg font-semibold">About</div>
-      <p className="text-gray-600 text-base sm:text-lg">
+      <div className="text- font-semibold">About</div>
+      <p className="text-gray-600  ">
         I am a self-taught full-stack developer skilled in building dynamic, real-time web applications. With a focus on Next.js, React.js, TypeScript, and Postgres, I create responsive and efficient websites. My work combines clean code and user-centered design, delivering seamless experiences across projects.{" "}
         <span className="text-blue-500 cursor-pointer" onClick={onProjectsClick}>
           Explore my projects
