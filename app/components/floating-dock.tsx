adb0f4e91fc61bedbe3c7d5da31e0193e31cc5cc
Anil Pal <anilcodes01@gmail.com>
2025-03-01 12:01:16 +0530

    added target blank

--- app/components/floating-dock.tsx
+++ app/components/floating-dock.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import {
   IconBrandGithub,
   IconBrandLinkedin,
@@ -19,7 +19,7 @@ import {
   useTransform,
 } from "framer-motion";
 import Link from "next/link";
-import { usePathname } from "next/navigation"; 
+import { usePathname } from "next/navigation";
 
 export const FloatingDock = () => {
   return (
@@ -33,6 +33,8 @@ type Link = {
   title: string;
   icon: React.ReactNode;
   href: string;
+  target?: string;
+  rel?: string;
 };
 
 type CustomMouseEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
@@ -47,7 +49,7 @@ const FloatingDockCore = () => {
     {
       title: "Projects",
       icon: <IconCode className="h-full w-full" />,
-      href: "/",
+      href: "#",
     },
     {
       title: "Blog",
@@ -57,27 +59,35 @@ const FloatingDockCore = () => {
     {
       title: "LinkedIn",
       icon: <IconBrandLinkedin className="h-full w-full" />,
-      href: "#",
+      href: "https://www.linkedin.com/in/anil-pal-532b692a6/",
+      target: "_blank",
+      rel: "noopener noreferrer",
     },
     {
       title: "Twitter",
       icon: <IconBrandX className="h-full w-full" />,
-      href: "#",
+      href: "https://x.com/Anilcodes01",
+      target: "_blank",
+      rel: "noopener noreferrer",
     },
     {
       title: "Mail",
       icon: <IconMail className="h-full w-full" />,
-      href: "/",
+      href: "#",
     },
     {
       title: "Resume",
       icon: <IconTerminal2 className="h-full w-full" />,
-      href: "/",
+      href: "https://drive.google.com/file/d/1xqHGsXko3tr8V9ktDSoDoemT_s10S2xs/view?usp=drive_link",
+      target: "_blank",
+      rel: "noopener noreferrer",
     },
     {
       title: "GitHub",
       icon: <IconBrandGithub className="h-full w-full" />,
-      href: "#",
+      href: "https://github.com/Anilcodes01",
+      target: "_blank",
+      rel: "noopener noreferrer",
     },
   ];
 
@@ -105,8 +115,8 @@ export const IconContainer = ({
 }) => {
   const ref = useRef<HTMLDivElement>(null);
   const [hovered, setHovered] = useState(false);
-  const pathname = usePathname(); 
-  const isActive = pathname === el.href; 
+  const pathname = usePathname();
+  const isActive = pathname === el.href;
 
   const distance = useTransform(mouseX, (val) => {
     const bounds = ref.current?.getBoundingClientRect() ?? { x: 0, width: 0 };
@@ -116,7 +126,11 @@ export const IconContainer = ({
   const widthTransform = useTransform(distance, [-150, 0, 150], [40, 80, 40]);
   const heightTransform = useTransform(distance, [-150, 0, 150], [40, 80, 40]);
 
-  const widthIconTransform = useTransform(distance, [-150, 0, 150], [20, 40, 20]);
+  const widthIconTransform = useTransform(
+    distance,
+    [-150, 0, 150],
+    [20, 40, 20]
+  );
   const heightIconTransform = useTransform(
     distance,
     [-150, 0, 150],
@@ -145,12 +159,13 @@ export const IconContainer = ({
     damping: 12,
   });
 
-
   const iconColorClass = isActive ? "text-orange-500" : "text-neutral-500";
 
   return (
     <Link
       href={el.href}
+      target={el.target}
+      rel={el.rel}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
@@ -160,38 +175,42 @@ export const IconContainer = ({
           width,
           height,
         }}
-        className={`flex relative items-center justify-center rounded-full ${hovered ? "bg-neutral-200" : ""}`}
+        className={`flex relative items-center justify-center rounded-full ${
+          hovered ? "bg-neutral-200" : ""
+        }`}
       >
         <AnimatePresence>
-        {hovered && (
-          <motion.div
-            initial={{
-              opacity: 0,
-              y: 10,
-              x:'-50%'
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              x:'-50%'
-            }}
-            exit={{
-              opacity: 0,
-              y: 2,
-              x:'-50%'
-            }}
-            className="absolute text-xs left-1/2 -translate-x-1/2 -top-8 inset-x-0 px-2 py-0.4 whitespace-pre bg-neutral-100 rounded-md w-fit text-neutral-500 z-10"
-          >
-            {el.title}
-          </motion.div>
-        )}
+          {hovered && (
+            <motion.div
+              initial={{
+                opacity: 0,
+                y: 10,
+                x: "-50%",
+              }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                x: "-50%",
+              }}
+              exit={{
+                opacity: 0,
+                y: 2,
+                x: "-50%",
+              }}
+              className="absolute text-xs left-1/2 -translate-x-1/2 -top-8 inset-x-0 px-2 py-0.4 whitespace-pre bg-neutral-100 rounded-md w-fit text-neutral-500 z-10"
+            >
+              {el.title}
+            </motion.div>
+          )}
         </AnimatePresence>
-       
+
         <motion.div
           style={{ width: widthIcon, height: heightIcon }}
           className={`flex items-center justify-center ${iconColorClass}`}
         >
-          {React.cloneElement(el.icon as React.ReactElement, { className: `h-full w-full ${iconColorClass}` })}
+          {React.cloneElement(el.icon as React.ReactElement, {
+            className: `h-full w-full ${iconColorClass}`,
+          })}
         </motion.div>
       </motion.div>
     </Link>
